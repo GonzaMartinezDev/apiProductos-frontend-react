70c3a90d437b2d746331f8845e0418ef96446e98
GonzaMartinezDev <gonzamartinezdev@gmail.com>
2023-02-27 18:04:47 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShowProducts from './components/ShowProducts';
 
 function App() {
   return (
     <div className="App">
-      <h1>Frontend React App</h1>
+      <h1>Frontend React App's</h1>
+      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ShowProducts/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/ShowProducts.jsx
+++ src/components/ShowProducts.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
 
 const ShowProducts = () => {
+
+
+    const endPoint = 'http://backend-laravel.test/api/products';
+    const [products, setProducts] = useState([]);
+
+
+
+    useEffect( () => {getProducts();}, []);
+
+    const getProducts = async () => {
+        const response = await axios.get(endPoint);
+        setProducts(response.data);
+    };
+
+
   return (
-    <div>ShowProducts</div>
+    <div>
+
+<h1>Show products</h1>
+
+    <h1>Show products</h1>
+    <table className='table table-striped'> 
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Stock</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.description}</td>
+                <td>{product.price}</td>
+                <td>{product.stock}</td>
+                <td>
+                  <button type="button" className="btn btn-primary">Edit</button>
+                  <button type="button" className="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+    </table>
+
+    </div>
   )
 }
 
